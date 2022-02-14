402191571f62fb1eac4efda09132c05c11a2ddcd
auoHan <710543268@qq.com>
2022-02-14 18:15:08 +0800

    deploy

---
+++ precache-manifest.df5ef4f749f2068cbfe331e7d17c1e0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c629471e5e5dced8d62",
+    "url": "/blueberry-website/css/app.fcdb8000.css"
+  },
+  {
+    "revision": "3f906f66f8f5e6a8b87e",
+    "url": "/blueberry-website/css/chunk-vendors.0eb005af.css"
+  },
+  {
+    "revision": "ecede41c4fd12d812f4968d2ed24f9a2",
+    "url": "/blueberry-website/index.html"
+  },
+  {
+    "revision": "1c629471e5e5dced8d62",
+    "url": "/blueberry-website/js/app.0fc934e2.js"
+  },
+  {
+    "revision": "3f906f66f8f5e6a8b87e",
+    "url": "/blueberry-website/js/chunk-vendors.72c3a46a.js"
+  },
+  {
+    "revision": "4c0c717e1a018043655a5d75684bf0c7",
+    "url": "/blueberry-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blueberry-website/robots.txt"
+  }
+]);
